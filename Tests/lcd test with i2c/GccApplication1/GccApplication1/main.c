1c3b51da4df0b7b9eb9f9e4028bddd69552b65f7
Mahdi Hasnat <mahdibuet3@gmail.com>
2021-07-02 02:47:01 +0600

    Tested lcd with i2c module
    problems : kala kom , bujhai jay na lekha , pot maximum diyeo lav hoyni

---
+++ Tests/lcd test with i2c/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,31 @@
+/*
+ * lcd_test_adc.c
+ *
+ * Created: 19-Feb-19 19:46:12
+ *  Author: David Prentice
+ */
+
+#define F_CPU 1000000
+#include <util/delay.h>
+#include <avr/io.h>
+#include "lcd.h"
+extern void lcd_backlight(char on);   //not in lcd.h
+
+#include <stdlib.h>       //for itoa()
+
+int main(void)
+{
+    lcd_init(LCD_ON_DISPLAY);
+    lcd_backlight(0);
+    _delay_ms(200);
+    lcd_backlight(1);
+    _delay_ms(200);
+    
+    while (1)
+    {
+        lcd_clrscr();
+        lcd_gotoxy(0, 0);
+        lcd_puts("Na re vai");
+        _delay_ms(250);
+    }
+}
