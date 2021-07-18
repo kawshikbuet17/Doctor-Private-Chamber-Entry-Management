bb7d1cb152f63f9527805873c9c503acb5d93ffd
kawshikbuet17 <kawshik.kumar.paul@gmail.com>
2021-07-19 01:36:44 +0600

    pulse test lcd port no updated

--- Tests/lcd + pulse sensor/pulse_sensor_application/code/main.cpp
+++ Tests/lcd + pulse sensor/pulse_sensor_application/code/main.cpp
@@ -9,12 +9,14 @@
 #define F_CPU 16000000UL // 16 MHz clock speed
 #endif
 
-#define D4 eS_PORTD4
-#define D5 eS_PORTD5
-#define D6 eS_PORTD6
-#define D7 eS_PORTD7
-#define RS eS_PORTC6
-#define EN eS_PORTC7
+#define D4 eS_PORTD5
+#define D5 eS_PORTD4
+#define D6 eS_PORTD3
+#define D7 eS_PORTD2
+#define RS eS_PORTD7
+#define EN eS_PORTD6
+
+#include "lcd_4bit.h"
 
 #include <avr/io.h>
 #include <util/delay.h>
